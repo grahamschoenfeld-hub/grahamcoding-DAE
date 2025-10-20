7bba54b42db03cc5d70f341a73a97f44b4034a7b
Grahamdamanfr <Graham.schoenfeld@icloud.com>
2025-10-20 16:03:09 -0400

    test

---
+++ python_1/Variables/IFELSE.py
@@ -0,0 +1,4 @@
+ADULT_AGE = 18
+userAge = 10
+if userAge < ADULT_AGE:
+    print("get the heck out lil boy")
---
+++ python_1/Variables/Variables.py
@@ -0,0 +1,9 @@
+Apples = 15
+Youth = "old very very old"
+otherName= 'dude you are NOT old'
+APPLES_PRICE = 150,234,557
+
+print(Apples)
+print(Youth)
+print(otherName)
+print(APPLES_PRICE)
