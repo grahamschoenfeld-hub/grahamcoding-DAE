136f0c702df5d93c81298662ea85232d55717a0f
Grahamdamanfr <Graham.schoenfeld@icloud.com>
2025-10-20 18:06:46 -0400

    i worked on sandbox and python and enjoyed some mine sweeper aswell

--- python_1/Variables/IFELSE.py
+++ python_1/Variables/IFELSE.py
@@ -1,4 +1,6 @@
 ADULT_AGE = 18
-userAge = 10
+userAge = 19
 if userAge < ADULT_AGE:
     print("get the heck out lil boy")
+else:
+    print ("ok fine you can come in but watch yourself")
---
+++ python_1/Variables/while.py
@@ -0,0 +1,7 @@
+currentNumbers = 1
+Endnumber = 11
+while currentNumbers < Endnumber:
+    print(currentNumbers)
+    currentNumbers = currentNumbers + 1
+if currentNumbers > Endnumber:
+    print("ok thats a wrap")
