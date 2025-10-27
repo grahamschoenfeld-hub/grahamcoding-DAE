1b859f12c604414a97842cd9fb487aa8081a71ac
Grahamdamanfr <Graham.schoenfeld@icloud.com>
2025-10-27 18:09:12 -0400

    im working on prompt engineering and im finishing up python and my projerct

--- python_1/Variables/sequences.py
+++
@@ -1,3 +0,0 @@
-myfavoriteCandy = ["chips ahoy, hershys, m&ms, reeses"]
-print( myfavoriteCandy[ 0 ])
-print (myfavoriteCandy[ 1 ])
--- python_1/Variables/while.py
+++ python_1/Variables/while.py
@@ -3,5 +3,3 @@ Endnumber = 11
 while currentNumbers < Endnumber:
     print(currentNumbers)
     currentNumbers = currentNumbers + 1
-if currentNumbers > Endnumber:
-    print("ok thats a wrap")
